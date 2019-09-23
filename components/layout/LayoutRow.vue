d103ae986e0f74a3bfed178159f1738429348a70
Dmitrii Alferev <dmitry@alferev.com>
2019-09-23 14:27:27 +0300

    New design footer

---
+++ components/layout/LayoutRow.vue
@@ -0,0 +1,39 @@
+<template>
+	<div :class="classes" :style="styles">
+		<slot/>
+	</div>
+</template>
+
+<script>
+/**
+ * Reason for having this component is that Vuetify 1.5.x does not support conditional
+ * css classes for padding & margin. This was added only in 2.x
+ */
+export default {
+	name: 'LayoutRow',
+	props: {
+		yPad: {
+			type: String,
+			default: '0'
+		},
+		classes: String
+	},
+	computed: {
+		styles () {
+			let xPad = this.$vuetify.breakpoint.name === 'xs'
+				? 8
+				: this.$vuetify.breakpoint.name === 'sm'
+					? 16
+					: 48
+
+			return {
+				'padding': `${this.yPad} ${xPad}px ${this.yPad} ${xPad}px`
+			}
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+
+</style>
