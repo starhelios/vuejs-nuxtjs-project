7056acb4e4a343885b03c721d010e15149d07d12
starhelios <starhelios94@gmail.com>
2019-09-23 14:27:27 +0300

    New design footer

--- components/layout/default/Footer.vue
+++ components/layout/default/Footer.vue
@@ -1,97 +1,39 @@
 <template>
-  <footer>
-    <v-content class="footer">
-      <v-container px-4>
-        <v-layout
-          row
-          wrap
-        >
-          <v-flex
-            xs12
-            sm12
-            md12
-            lg12
-            xl12
-            offset-xs0
-            offset-sm0
-            offset-md0
-            offset-lg0
-            offset-xl0
-            mt-5
-            mb-5
-          >
-            <v-divider />
-          </v-flex>
-          <v-flex
-            xs10
-            sm10
-            md10
-            lg10
-            xl10
-            offset-xs1
-            offset-sm1
-            offset-md0
-            offset-lg0
-            offset-xl0
-          >
-            <div class="h3 follow-us">
-              {{ heading }}
-            </div>
-          </v-flex>
-          <v-flex
-            xs11
-            sm11
-            md6
-            offset-sm1
-            offset-md0
-            mt-4
-          >
-            <div class="social-media text-xs-left text-md-left">
-              <v-layout
-                row
-                wrap
-              >
-                <v-flex
-                  v-for="(link, i) in links"
-                  :key="`links${i}`"
-                  shrink
-                  offset-xs1
-                  :offset-sm0="i === 0"
-                  :offset-sm1="i !== 0"
-                  :offset-md0="i === 0"
-                  :offset-lg0="i === 0"
-                  :offset-xl0="i === 0"
-                >
-                  <a
-                    :href="link.href"
-                    target="_blank"
-                  >{{ link.text }}</a>
-                </v-flex>
-              </v-layout>
-            </div>
-          </v-flex>
-          <v-flex
-            class="footer-text text-xs-center"
-            xs12
-            sm12
-            md6
-            mt-4
-            mb-4
-            text-xs-center
-            text-sm-center
-            text-md-right
-          >
-            {{ copyright }}
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
-  </footer>
+	<footer>
+		<LayoutRow classes="footer">
+			<v-container>
+				<v-layout row wrap justify-space-around align-center>
+					<v-flex class="hidden-xs-only pt-1">
+						<img src="/img/LogoWhite.svg">
+					</v-flex>
+
+					<v-flex xs12 sm4 class="text-xs-center">
+						<a v-for="(link, i) in links"
+							:key="`links${i}`"
+							:href="link.href"
+							target="_blank"
+							:class="{ 'ml-2': i > 0 }">
+							{{ link.text }}
+						</a>
+					</v-flex>
+
+					<v-flex xs12 sm4 class="text-xs-center text-md-right copyright">
+						{{ copyright }}
+					</v-flex>
+				</v-layout>
+			</v-container>
+		</LayoutRow>
+	</footer>
 </template>
+
 <script>
 import { mapGetters } from 'vuex'
+import LayoutRow from '@/components/layout/LayoutRow'
 
 export default {
+	components: {
+		LayoutRow
+	},
   computed: {
     ...mapGetters({
       heading: 'footer/getHeading',
@@ -101,3 +43,42 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+@import "@/assets/css/vars.scss";
+
+.footer {
+	background-color: rgb(133, 196, 205);
+	color: white;
+	font-size: 110%;
+
+	@media screen and (max-width: 600px) {
+		padding-top: 36px !important;
+		padding-bottom: 36px !important;
+	}
+
+	@media screen and (min-width: 600px) {
+		padding-top: 20px !important;
+		padding-bottom: 20px !important;
+	}
+
+	& a {
+		color: white;
+		text-decoration: none;
+		
+
+		&:hover {
+			color: rgb(251, 243, 212);
+		}
+	}
+
+	& .copyright {
+		font-weight: 100;
+		font-family: serif !important;
+
+		@media screen and (max-width: 600px) {
+			margin-top: 16px;
+		}		
+	}
+}
+</style>
