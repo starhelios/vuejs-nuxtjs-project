df4d9bd466baebf6433c889ee59bc52cf60db1e1
Dmitrii <Alferev>
2019-09-26 23:46:25 +0300

    Different layout for brand images

--- components/lists/BrandImages.vue
+++ components/lists/BrandImages.vue
@@ -1,80 +1,24 @@
 <template>
-  <v-container py-0>
+  <v-container
+    py-0
+    class="brand-images"
+  >
     <v-layout
       row
       wrap
-      justify-content-around
-      class="justify-space-around"
+      :justify-space-around="$vuetify.breakpoint.smAndUp"
+      :justify-space-between="$vuetify.breakpoint.xsOnly"
     >
-      <v-flex
-        v-for="(image, i) in getImages()"
-        :key="`image${i}`"
-        class="pa-1 brand shrink align-center"
+      <img
+        src="/img/companies/logos-mobile.svg"
+        class="hidden-sm-and-up mb-4"
+        style="max-width: 100%"
       >
-        <img
-          height="25px"
-          :src="getImagePath(image)"
-        >
-      </v-flex>
+      <img
+        src="/img/companies/logos-desktop.svg"
+        class="hidden-xs-only"
+        style="max-width: 100%"
+      >
     </v-layout>
   </v-container>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      isHydrated: false,
-      images: [
-        'Google.svg',
-        'P&G.svg',
-        'Zelle.svg',
-        'Nike.svg',
-        'Target.svg',
-        'UCSF.svg',
-        'Lexus.svg'
-      ]
-    }
-  },
-
-  computed: {
-    mdAndUp() {
-      return this.isHydrated
-        ? this.$vuetify.breakpoint.mdAndUp
-        : false
-    },
-    xs() {
-      return this.isHydrated
-        ? this.$vuetify.breakpoint.xs
-        : false
-    },
-    smAndDown() {
-      return this.isHydrated
-        ? this.$vuetify.breakpoint.smAndDown
-        : false
-    }
-  },
-
-  mounted() {
-    this.isHydrated = true
-  },
-
-  methods: {
-    getImagePath(image) {
-      return 'best-prictice/' + image
-    },
-
-    getImages() {
-      return this.images
-    },
-
-    getImageClasses(i) {
-      return {
-        'small': this.smAndDown && i >= 4,
-        'position': i >= 3 && this.mdAndUp,
-        'position-small': this.mdAndUp && [4, 6].indexOf(i) > -1
-      }
-    }
-  }
-}
-</script>
